aca4551c27699a079175307e9198b19c46d84877
Ma Chun Ho <ma.rocky.oa@outlook.com>
2025-10-21 21:55:04 +0800

    test: Add and Pass all move command test cases

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -37,7 +37,7 @@ namespace adas
         ASSERT_EQ(executor->Query(), target);
     }
 
-    // 测试用例3
+    // 测试用例3,测试Execute方法,朝向E,起点(0,0),执行M命令,期望结果为(1,0,E)
     TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
     {
         // given给定一个executor
@@ -50,4 +50,88 @@ namespace adas
         const Pose target = {1, 0, 'E'};
         ASSERT_EQ(executor->Query(), target);
     }
+
+    // 测试用例4, 测试Execute方法,朝向W,起点(0,0),执行M命令,期望结果为(-1,0,W)
+    TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+
+        executor->Execute("M");
+
+        const Pose target = {-1, 0, 'W'};
+        ASSERT_EQ(executor->Query(), target);
+    }
+
+    // 测试用例5, 测试Execute方法,朝向N,起点(0,0),执行M命令,期望结果为(0,1,N)
+    TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+
+        executor->Execute("M");
+
+        const Pose target = {0, 1, 'N'};
+        ASSERT_EQ(executor->Query(), target);
+    }
+
+    // 测试用例6, 测试Execute方法,朝向S,起点(0,0),执行M命令,期望结果为(0,-1,S)
+    TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+
+        executor->Execute("M");
+
+        const Pose target = {0, -1, 'S'};
+        ASSERT_EQ(executor->Query(), target);
+    }
+
+    // 以上是M指令相关的测试代码
+
+    // 测试用例7, 测试Execute方法,朝向E,起点(0,0),执行L命令,期望结果为(0,0,N)
+    TEST(ExecutorTest, should_return_facing_N_given_command_is_L_and_facing_is_E)
+    {
+        // std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+
+        // executor->Execute("L");
+
+        // const Pose target = {0, -1, 'S'};
+        // ASSERT_EQ(executor->Query(), target);
+    }
+
+    // 测试用例8, 测试Execute方法,朝向W,起点(0,0),执行L命令,期望结果为(0,0,S)
+    TEST(ExecutorTest, should_return_facing_W_given_command_is_L_and_facing_is_N)
+    {
+    }
+
+    // 测试用例9, 测试Execute方法,朝向N,起点(0,0),执行L命令,期望结果为(0,0,W)
+    TEST(ExecutorTest, should_return_facing_S_given_command_is_L_and_facing_is_W)
+    {
+    }
+
+    // 测试用例10, 测试Execute方法,朝向S,起点(0,0),执行L命令,期望结果为(0,0,E)
+    TEST(ExecutorTest, should_return_facing_E_given_command_is_L_and_facing_is_S)
+    {
+    }
+
+    // 以上是L指令相关测试代码
+
+    // 测试用例11, 测试Execute方法,朝向E,起点(0,0),执行R命令,期望结果为(0,0,S)
+    TEST(ExecutorTest, should_return_facing_S_given_command_is_R_and_facing_is_E)
+    {
+    }
+
+    // 测试用例12, 测试Execute方法,朝向W,起点(0,0),执行R命令,期望结果为(0,0,N)
+    TEST(ExecutorTest, should_return_facing_W_given_command_is_R_and_facing_is_S)
+    {
+    }
+
+    // 测试用例13, 测试Execute方法,朝向N,起点(0,0),执行R命令,期望结果为(0,0,E)
+    TEST(ExecutorTest, should_return_facing_N_given_command_is_R_and_facing_is_W)
+    {
+    }
+
+    // 测试用例14, 测试Execute方法,朝向S,起点(0,0),执行R命令,期望结果为(0,0,W)
+    TEST(ExecutorTest, should_return_facing_E_given_command_is_R_and_facing_is_N)
+    {
+    }
+    // 以上是R指令相关的测试代码
 }
